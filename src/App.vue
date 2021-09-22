9fd5af243a12d94b84e6ce3a6ac4a51ee550a427
Walter <walter@quomi.it>
2021-09-22 12:18:22 +0200

    imported all the components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
-    
-  </div>
+  <main></main>
 </template>
 
 <script>
+import Vue from 'vue'
+
+import AddCategory from './components/AddCategory.vue'
+import AddBill from './components/AddBill.vue'
+import NavBar from './components/NavBar.vue'
+import Chart from './components/Chart.vue'
+import BillsTable from './components/BillsTable.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    AddCategory,
+    AddBill,
+    NavBar,
+    Chart,
+    BillsTable
+  }
+}
 
 </script>
 
