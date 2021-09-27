3f02a11b6ccf2f6fd9bcfbe1aee2957b82473c7c
Walter <walter@quomi.it>
2021-09-27 12:12:44 +0200

    added Tailwind css classes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul>
-    <li v-for="category in categories" :key="category">{{ category }}</li>
-    <li @click="triggerShowAddCategory">+</li>
+  <ul class="list-reset inline flex justify-center border-b-4 mb-0">
+    <li class="top_menu_items" v-for="category in categories" :key="category">{{ category }}</li>
+    <li class="top_menu_items" @click="triggerShowAddCategory">+</li>
   </ul>
 </template>
 
@@ -15,3 +15,9 @@ export default {
   props: ['categories'],
 }
 </script>
+
+<style lang="postcss" scoped>
+.top_menu_items {
+  @apply  p-4 inline bg-gray-200 hover:bg-gray-400 uppercase font-black cursor-pointer font-mono text-3xl;
+}
+</style>
