0e9c99943c9ffe78d610b752cca71dfa6ee57b37
Walter <walter@quomi.it>
2021-09-27 20:20:44 +0200

    style minor fix

--- src/components/AddBill.vue
+++ src/components/AddBill.vue
@@ -3,14 +3,15 @@
     <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
       <div class="mb-4">
         <h1 class="text-gray-800">Enter a new bill</h1>
+        <p></p>
         <div class="flex mt-4">
           <datepicker
-            wrapper-class="shadow border rounded text-gray-700"
-            input-class="py-2 px-2"
+            wrapper-class="shadow appearance-none border rounded text-gray-700"
+            input-class="w-full w-full py-2 px-3 mr-4 "
             v-model="date"
           ></datepicker>
 
-          <select class="shadow border rounded mx-4" v-model="category">
+          <select v-model="category">
             <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
           </select>
           
@@ -20,7 +21,7 @@
             v-model="amount"
           >
           <button
-            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 border-teal hover:bg-teal-700"
+            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 text-white border-teal hover:text-white hover:bg-teal-700"
             @click="handleClick"
           >Add</button>
         </div>
@@ -30,38 +31,38 @@
 </template>
 
 <script>
-  import Datepicker from 'vuejs-datepicker'
+import Datepicker from "vuejs-datepicker";
 
-  export default {
-    name: 'AddBill',
-    data: function () {
-      return {
-        date: new Date(),
-        category: this.categories[0],
-        amount: 0,
+export default {
+  name: "AddBill",
+  props: ["categories"],
+  data: function() {
+    return {
+      date: new Date(),
+      category: this.categories[0],
+      amount: 0
+    };
+  },
+  components: {
+    Datepicker
+  },
+  methods: {
+    handleClick: function() {
+      if (!this.date) {
+        alert("Enter a date");
+        return;
       }
-    },
-    components: {
-      Datepicker,
-    },
-    methods: {
-      handleClick: function() {
-        if (!this.date) {
-          alert("Please, enter a date!")
-          return
-        }
-        if (!this.amount) {
-          alert("Please, enter an amount!")
-          return
-        }
+      if (!this.amount) {
+        alert("Enter an amount");
+        return;
+      }
 
-        this.$emit('addBill', {
-          date: this.date,
-          category: this.category,
-          amount: parseInt(this.amount, 10)
-        })
-      }
-    },
-    props: ['categories'],
+      this.$emit("addBill", {
+        date: this.date,
+        category: this.category,
+        amount: parseInt(this.amount, 10)
+      });
+    }
   }
+};
 </script>
--- src/components/AddCategory.vue
+++ src/components/AddCategory.vue
@@ -11,7 +11,7 @@
             v-model="category"
           />
           <button
-            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 text-black border-teal hover:text-white hover:bg-teal-700"
+            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 text-black border-teal hover:bg-teal-700"
             @click="handleClick"
           >
             Add
