3c13f376afaf5f820c5df2efd352cb64df32875b
Walter <walter@quomi.it>
2021-09-27 11:49:41 +0200

    showed in navbar component all the categories that the user add

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,17 @@
 <template>
-  <p>Navbar</p>
+  <ul>
+    <li v-for="category in categories" :key="category">{{ category }}</li>
+    <li @click="triggerShowAddCategory">+</li>
+  </ul>
 </template>
+
+<script>
+export default {
+  methods: {
+    triggerShowAddCategory() {
+      this.$emit('triggerShowAddCategory')
+    }
+  },
+  props: ['categories'],
+}
+</script>
