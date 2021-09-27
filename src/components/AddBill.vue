9971d4d1035356621f32e386cd727a2f9bf831f1
Walter <walter@quomi.it>
2021-09-27 19:04:17 +0200

    added css to style

--- src/components/AddBill.vue
+++ src/components/AddBill.vue
@@ -1,16 +1,30 @@
 <template>
-  <div>
-    <h1>Enter a new bill</h1>
-    <p></p>
-    <div>
-      <select v-model="category">
-        <option v-for="category in categories" :value="category" :key="category">
-          {{ category }}
-        </option>
-      </select>
-      <datepicker v-model="date"></datepicker>
-      <input placeholder="Set amount" v-model="amount">
-      <button @click="handleClick">Add</button>
+  <div class="h-100 w-full flex items-center justify-center font-sans">
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+      <div class="mb-4">
+        <h1 class="text-gray-800">Enter a new bill</h1>
+        <div class="flex mt-4">
+          <datepicker
+            wrapper-class="shadow border rounded text-gray-700"
+            input-class="py-2 px-2"
+            v-model="date"
+          ></datepicker>
+
+          <select class="shadow border rounded mx-4" v-model="category">
+            <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
+          </select>
+          
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-700"
+            placeholder="Set amount"
+            v-model="amount"
+          >
+          <button
+            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 border-teal hover:bg-teal-700"
+            @click="handleClick"
+          >Add</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
