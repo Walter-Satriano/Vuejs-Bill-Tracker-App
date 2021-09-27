83704e84370a349ee3d8ae2a0247347d59937bea
Walter <walter@quomi.it>
2021-09-27 17:35:55 +0200

    imported the AddBill component. setted up the the local storage to save data

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <main>
     <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory" />
     <div v-else>
-      <NavBar :categories="categories" v-on:triggerShowAddCategory="triggerShowAddCategory" />
-      <div class="container flex">
-        <div class="w-1/2">
-          <BillsTable />
-        </div>
-        <div class="w-1/2">
-          <Chart :bills="activeBills" />
-        </div>
-      </div>  
+      <AddBill v-if="shouldShowAddBill" :categories="categories" v-on:addBill="addBill" />
+      <div v-else>
+        <NavBar :categories="categories" v-on:triggerShowAddCategory="triggerShowAddCategory" />
+        <div class="container flex">
+          <div class="w-1/2">
+            <BillsTable />
+          </div>
+          <div class="w-1/2">
+            <Chart :bills="activeBills" />
+          </div>
+        </div>  
+      </div>
     </div>
   </main>
 </template>
@@ -30,7 +33,8 @@ export default {
     return {
       bills: [],
       categories: [],
-      shouldShowAddCategory: false
+      shouldShowAddCategory: false,
+      shouldShowAddBill: true
     }
   },
   components: {
@@ -48,21 +52,31 @@ export default {
     triggerShowAddCategory() {
       this.shouldShowAddCategory = true
     },
+    addBill(bill) {
+      this.bills.push(bills)
+      this.shouldShowAddBill = false
+    }
   },
   watch: {
+    bills() {
+      localStorage.setItem('bills', JSON.stringify(this.bills))
+    },
     categories() {
       localStorage.setItem('categories', JSON.stringify(this.categories))
     }
   },
   mounted() {
+    if (localStorage.getItem('bills')) {
+      this.bills = JSON.parse(localStorage.getItem('bills'))
+    }
+
     if (localStorage.getItem('categories')) {
       this.categories = JSON.parse(localStorage.getItem('categories'))
     }
 
-    if (!this.categories.length) {
+    if (!this.bills.length && !this.categories.length) {
       this.shouldShowAddCategory = true
     }
-    
   }
 }
 
