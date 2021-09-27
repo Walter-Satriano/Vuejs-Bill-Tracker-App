7db6135933ecbb3c078aa7a18689f5f16e544b70
Walter <walter@quomi.it>
2021-09-27 19:22:55 +0200

    structure if the table that will show all the bills

--- src/components/BillsTable.vue
+++ src/components/BillsTable.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>BillsTable</p>
+  <table class="table">
+    <thead class="bg-blue-400 text-white">
+      <tr>
+        <th scope="col">Date</th>
+        <th scope="col">Amount</th>
+        <th scope="col">Category</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>date
+      <tr vamount-for="(bidatell, index) in bills" :key="index" class="p-4">
+        <td>{{ bill.date }}</td>
+        <td>{{ bill.amount }} €</td>
+        <td>{{ bill.category }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
