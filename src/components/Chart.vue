e3b8c034b628eb32ba0aaca13a164597b10b1313
Walter <walter@quomi.it>
2021-09-28 18:51:56 +0200

    added function that accepts an array of bills, filters out bills older than 1 year, and returns the total amount we paid each month

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,7 +1,10 @@
 <script>
   import { Bar } from 'vue-chartjs';
-  //import moment from 'moment';
+  import moment from 'moment';
 
+
+  /* months is the list of month names, and based on the current month,
+  which we determine from the current date, I fill the orderedMonths array, and I return it */
   const last12Months = () => {
     const months = [
       'January',
@@ -30,7 +33,29 @@
     }
 
     return orderedMonths
+  }
 
+  /* This function accepts an array of bills, filters out bills older than 1 year,
+  and returns the total amount we paid each month */
+  const processBills = (bills) => {
+    const oneYearAgo = moment().subtract(1, 'years')
+    const months = last12Months()
+    const monthsWithValues = new Array(12).fill(0)
+
+    for (const month of monthsWithValues) {
+      monthsWithValues[month] = 0
+    }
+
+    for (const bill of bills) {
+      if (moment(bill.date).isSameOrBefore(oneYearAgo)) {
+        continue
+      }
+      const monthName = moment(bill.date).format('MMMM')
+      const indexOfMonth = months.indexOf(monthName)
+      monthsWithValues[indexOfMonth] += parseInt(bill.amount)
+    }
+
+    return monthsWithValues
   }
 
   export default {
