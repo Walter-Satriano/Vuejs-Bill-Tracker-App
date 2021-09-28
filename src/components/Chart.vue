085e4b868be434bcbdfa54af3f0811fc168ebeb8
Walter <walter@quomi.it>
2021-09-28 18:29:12 +0200

    added function: months is the list of month names, and based on the current month, which we determine from the current date, I fill the orderedMonths array, and I return it.

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,3 +1,40 @@
-<template>
-  <p>Chart</p>
-</template>
+<script>
+  import { Bar } from 'vue-chartjs';
+  //import moment from 'moment';
+
+  const last12Months = () => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const today = new Date()
+    const orderedMonths = []
+    let month = today.getMonth() + 1
+    
+    if (month === 12) month = 0  //correctly handle december
+
+    for (let i = 0; i < 12; i++) {
+      orderedMonths.push(months[month])
+      month === 11 ? (month = 0) : month++
+    }
+
+    return orderedMonths
+
+  }
+
+  export default {
+    extends: Bar,
+
+  }
+</script>
