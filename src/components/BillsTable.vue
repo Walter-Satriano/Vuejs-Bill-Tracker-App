3cc3f7955b7ed9c3b4b2b9c0e7c0ef0ce6ce0684
Walter <walter@quomi.it>
2021-09-28 17:11:32 +0200

    added a button to remove a bill and a function that manage it

--- src/components/BillsTable.vue
+++ src/components/BillsTable.vue
@@ -18,6 +18,7 @@
         <td>{{ bill.date | moment("MMM D YYYY")}}</td>
         <td>€ {{ bill.amount }}</td>
         <td>{{ bill.category }}</td>
+        <td><button @click="removeBill(index)">X</button></td>
       </tr>
     </tbody>
   </table>
@@ -29,6 +30,10 @@
     methods: {
       triggerShowAddBill: function() {
         this.$emit('triggerShowAddBill')
+      },
+
+      removeBill: function(index) {
+        this.$emit('removeBill', index)
       }
     },
     props: ['bills']
