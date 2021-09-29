1b839ce182070261c56eb77f4f6f2f2a6ed0b1c0
Walter <walter@quomi.it>
2021-09-29 12:20:53 +0200

    added prop activeCategory, setted a dynamic :class to change color when a category is clicked, added a All button with the function that clear the other selected category when is clicked and show all the bills in the table

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,44 @@
 <template>
   <ul class="list-reset inline flex justify-center border-b-4 mb-0">
-    <li class="top_menu_items" v-for="category in categories" :key="category">{{ category }}</li>
-    <li class="top_menu_items" @click="triggerShowAddCategory">+</li>
+
+    <li
+      class="top_menu_items"
+      :class="[ activeCategory === '' ? 'bg-gray-600' : 'bg-gray-200' ]"
+      @click="clearActiveCategory()"
+    >All</li>
+
+    <li
+      class="top_menu_items"
+      :class="[ activeCategory === category ? 'bg-gray-600' : 'bg-gray-200' ]"
+      v-for="category in categories"
+      :key="category"
+      @click="setActiveCategory(category)"
+    >{{ category }}</li>
+
+    <li class="top_menu_items bg-gray-200" @click="triggerShowAddCategory">+</li>
+
   </ul>
 </template>
 
 <script>
 export default {
+  props: ['categories', 'activeCategory'],
   methods: {
     triggerShowAddCategory() {
       this.$emit('triggerShowAddCategory')
+    },
+    clearActiveCategory() {
+      this.$emit('clearActiveCategory')
+    },
+    setActiveCategory(category) {
+      this.$emit('setActiveCategory', category)
     }
   },
-  props: ['categories'],
 }
 </script>
 
 <style lang="postcss" scoped>
 .top_menu_items {
-  @apply  p-4 inline bg-gray-200 hover:bg-gray-400 uppercase font-black cursor-pointer font-mono text-3xl;
+  @apply  p-4 inline hover:bg-gray-400 uppercase font-black cursor-pointer font-mono text-3xl;
 }
 </style>
