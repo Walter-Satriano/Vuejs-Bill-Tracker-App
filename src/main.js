cf7df4f5c15aa8e932437c465c55bb408688dd94
Walter <walter@quomi.it>
2021-10-04 19:54:06 +0200

    installed font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import "./index.css"
+
+library.add(faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false;
 
