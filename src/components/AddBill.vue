cf86728a15a95ab2fb1a26a4ed60797ac2b67435
Walter <walter@quomi.it>
2021-10-07 19:07:10 +0200

    fixed with better style

--- src/components/AddBill.vue
+++ src/components/AddBill.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="h-100 w-full flex items-center justify-center font-sans">
-    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-      <div class="mb-4">
-        <h1 class="text-gray-800">Enter a new bill</h1>
-        <p></p>
-        <div class="flex mt-4">
+  <div class="flex items-center justify-center font-sans">
+    <div class="w-full sm:w-3/5 lg:w-1/2 xl:w-2/5 border-2 border-blue-500 rounded shadow p-6 m-4 text-center">
+      <div class="font-bold">
+        <h5 class="text-xl sm:text-2xl">Enter a new bill</h5>
+        <div class="text-base sm:text-lg mt-7">
+          <p class="text-left mb-1">Select a date:</p>
           <datepicker
-            wrapper-class="shadow appearance-none border rounded text-gray-700"
-            input-class="w-full w-full py-2 px-3 mr-4 "
+            wrapper-class="shadow border-2 border-blue-500 rounded"
+            input-class="w-full py-2 px-3"
             v-model="date"
           ></datepicker>
 
-          <select v-model="category">
+          <p class="text-left mb-1 mt-5">Select a date:</p>
+          <select v-model="category" class="w-full shadow border-2 border-blue-500 rounded py-2 px-3">
             <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
           </select>
-          
+
+          <p class="text-left mb-1 mt-5">Set an amount:</p>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-700"
-            placeholder="Set amount"
+            class="w-full shadow border-2 border-blue-500 rounded py-2 px-3"
             v-model="amount"
           >
           <button
-            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 border-teal hover:bg-teal-700"
+            class="add-bill-btn"
             @click="handleClick"
           >Add</button>
         </div>
@@ -62,7 +63,14 @@ export default {
         category: this.category,
         amount: parseInt(this.amount, 10)
       });
-    }
+    },
   }
 };
 </script>
+
+<style lang="postcss" scoped>
+  .add-bill-btn {
+    @apply rounded-full py-2 px-4 mt-5 bg-blue-500 hover:bg-blue-700
+      text-white text-center font-bold text-lg sm:text-2xl
+  }
+</style>
--- src/components/AddCategory.vue
+++ src/components/AddCategory.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="h-100 w-full flex items-center justify-center font-sans">
-    <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-      <div class="mb-4">
-        <h1 class="text-gray-800">Enter a category of bills</h1>
-        <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
-        <div class="flex mt-4">
+  <div class="flex items-center justify-center font-sans">
+    <div class="w-full sm:w-3/5 lg:w-1/2 xl:w-2/5 border-2 border-green-500 rounded shadow p-6 m-4 text-center">
+      <div class="font-bold">
+        <h5 class="text-xl sm:text-2xl">Enter a category of bills</h5>
+        <p class="text-base sm:text-lg">E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+        <div class="mt-7">
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-700 capitalize"
+            class="w-full shadow border-2 border-green-500 rounded py-2 px-3 capitalize"
             placeholder="Add category"
             v-model="category"
-          />
+          >
           <button
-            class="flex-no-shrink p-2 border-2 rounded bg-teal-700 text-black border-teal hover:bg-teal-700"
+            class="add-bill-btn"
             @click="handleClick"
           >
             Add
@@ -43,3 +43,10 @@ export default {
   },
 }
 </script>
+
+<style lang="postcss" scoped>
+  .add-bill-btn {
+    @apply rounded-full py-2 px-4 mt-5 bg-green-500 hover:bg-green-700
+      text-white text-center font-bold text-lg sm:text-2xl
+  }
+</style>
